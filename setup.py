5fa04e4b4bcefa38e1720241ac53ddbed3daf18f
arcward <git@edawrd.sh>
2017-04-20 01:03:40 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='echopy',
+    version='0.0.1',
+    author='Edward Wells',
+    author_email='git@edward.sh',
+    description="Alexa Skills Kit SDK for Python 3.6",
+    license='MIT',
+    keywords='Amazon AWS Alexa Skills Kit ASK py3 python3.6 lambda',
+    url='https://github.com/arcward/echopy',
+    packages=['echopy']
+)
