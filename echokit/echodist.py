146f7abd1b654ed78937905405e53d647317c02e
Edward Wells <git@edward.sh>
2017-08-08 18:57:26 -0400

    fixed echodist annoyances
    Updated naming of output files/directories to use the current timestamp to avoid possible conflicts with existing paths, also removed user input prompt 
    
    Removed `--dir` argument because it was the only one anyway, set as positional named `project_dir`
    
    Clarified some help messages and added more console output for read/write/copy operations

--- echokit/echodist.py
+++ echokit/echodist.py
@@ -1,87 +1,81 @@
-"""Command-line utility to create Lambda deployment ZIP files"""
+"""Utility to bundle echokit into a lambda function for deployment as ZIP
+
+Deploying an AWS Lambda function using a ZIP archive requires dependencies
+to be included as well. Specifically, this utility copies the specified
+directory tree into a temporary one, adds an *echokit* directory at the
+top level of that structure, copies the current echokit install into that
+directory, and zips it all up.
+
+So rather than explicitly creating this directory in a project and possibly
+running into conflicts between that and the version installed via *pip*,
+the installed version will always be copied into the final ZIP.
+"""
 import argparse
 import os
 import echokit
 import shutil
 import zipfile
-
-
-def _zipdir(path, ziph):
-    # Modified from example at:
-    # http://stackoverflow.com/questions/1855095/how-to-create-a-zip-archive-of-a-directory
-    # Modified to use relative paths instead of recreating the entire
-    #  directory structure in the ZIP file. Ex: Zipping /home/me/project
-    #  would create paths like /home/me/project/test.txt
-    # Now this will cause /home/me/project/test.txt to be at the top of
-    # the ZIP directory structure
-    os.chdir(path)
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            rel_path = os.path.relpath(os.path.join(root), os.getcwd())
-            output_path = os.path.join(rel_path, file)
-            ziph.write(output_path)
-
-
-def _copy_dir(project_path):
-    """Copies given path to tmp-{project_path}"""
-    tmp_path = f"tmp-{os.path.basename(os.path.normpath(project_path))}"
-    print(f"Copying project directory to temporary path: {tmp_path}")
-    shutil.copytree(project_path, tmp_path)
-    shutil.copytree(os.path.dirname(echokit.__file__),
-                    os.path.join(tmp_path, 'echokit'))
-    return os.path.join(os.path.abspath(os.getcwd()), tmp_path)
-
-
-def _filename_prompt(file_name):
-    """Prompt user to rename output file if it already exists"""
-    rename = input(f"File {file_name} already exists. Rename? [Y/n] ")
-    rename = rename.strip()
-    rename = rename.upper()
-    if rename == 'Y':
-        new_filename = input("Enter new filename: ")
-    else:
-        exit()
-    path = os.path.join(os.path.abspath(os.getcwd()), new_filename)
-    if os.path.exists(path):
-        _filename_prompt(new_filename)
-    else:
-        return path
+from datetime import datetime
 
 
 def echodist():
-    description = "Creates a deployment package for AWS Lambda"
-    epilog = ("Specify the top level of your project with --dir.\nFor "
-              "example, if __init__.py is located at  "
-              "/home/me/project/project/__init__.py, you would specify "
-              "/home/me/proj_root/project\nThis will create "
-              "a 'project.zip' file in your current directory.")
-    parser = argparse.ArgumentParser(prog='echodist', description=description,
+    prog = "echodist"
+    desc = ("Bundles the specified directory into a ZIP archive with echokit "
+            "for upload to AWS Lambda")
+    epilog = ("Specify the base directory of your project. A ZIP file will "
+              "be generated in the current working directory containing "
+              "the contents of that directory, as well as echokit at the "
+              "top level to enable import statements.")
+    parser = argparse.ArgumentParser(prog=prog, description=desc,
                                      epilog=epilog)
-    parser.add_argument('--dir', required=True,
-                        help='Directory containing your project')
+    parser.add_argument('project_dir', help="Project base directory")
     args = parser.parse_args()
 
-    out_dir = os.path.abspath(args.dir)
-    if not os.path.exists(out_dir):
-        raise NotADirectoryError(f"Invalid project path: {out_dir}")
-    print(f"Creating Lambda deployment package from:\n\t{out_dir}")
+    # Initial timestamp used to help identify output paths
+    timestamp = datetime.now().strftime("%d-%m-%y-%H%M%S")
 
-    # If /home/me/project/, then create project.zip in the current working dir
-    file_name = f"{os.path.basename(os.path.normpath(out_dir))}.zip"
-    file_path = os.path.join(os.path.abspath(os.getcwd()), file_name)
-    print(f"Creating deployment package in:\n\t{file_path}")
-    if os.path.exists(file_path):
-        file_path = _filename_prompt(file_name)
+    if not os.path.isdir(args.project_dir):
+        raise NotADirectoryError(f"Invalid project path: {args.project_dir}")
 
-    # Copy to tmp directory, cd in and create the archive
-    tmp_project_dir = _copy_dir(out_dir)
-    output_file_path = os.path.join(os.path.abspath(os.getcwd()), file_path)
+    # Last directory in path is assumed to be the project for naming purposes
+    project_base = os.path.basename(os.path.abspath(args.project_dir))
+    # temporary directory to hold copy of target project
+    temp_path = f"{timestamp}_{project_base}"
+    temp_dir = os.path.join(os.getcwd(), temp_path)
+    # zip file to be exported
+    zip_filename = f"{project_base}_{timestamp}.zip"
+    zip_path = os.path.join(os.getcwd(), zip_filename)
+    # installation path for the current interpreter
+    echokit_path = os.path.dirname(echokit.__file__)
+    echokit_target = os.path.join(temp_dir, "echokit")
 
-    dist_zip = zipfile.ZipFile(output_file_path, 'w', zipfile.ZIP_DEFLATED)
-    _zipdir(tmp_project_dir, dist_zip)
-    dist_zip.close()
+    if os.path.exists(temp_dir):
+        raise IsADirectoryError(f"Directory exists! {temp_dir}")
+    if os.path.exists(zip_path):
+        raise FileExistsError(f"File exists!: {zip_path}")
 
-    print("Deployment package created")
-    print(f"Removing temporary directory:\n\t{tmp_project_dir}")
-    shutil.rmtree(tmp_project_dir)
-    print("Done")
+    # Copies the target project directory tree into a temporary
+    # one, then copies echokit from wherever it's installed, into
+    # a new 'echokit/' directory at the top of the target project copy
+    print(f"Copying project to temporary directory {temp_dir}")
+    shutil.copytree(args.project_dir, temp_dir)
+    print(f"Copying echokit lib from {echokit_path} to {echokit_target}")
+    shutil.copytree(echokit_path, echokit_target)
+    print(f"Creating zip file: {zip_path}")
+    zip_file = zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED)
+
+    current_dir = os.getcwd()
+    os.chdir(temp_dir)
+    for root, dirs, files in os.walk(temp_dir):
+        for file in files:
+            rel_path = os.path.relpath(os.path.join(root), os.getcwd())
+            out_path = os.path.join(rel_path, file)
+            print(f"Writing: {out_path}")
+            zip_file.write(out_path)
+    zip_file.close()
+
+    print(f"Created: {zip_path}")
+    print(f"Cleaning up, deleting temporary directory: {temp_dir}")
+    os.chdir(current_dir)
+    shutil.rmtree(temp_dir)
+    print("Done!")
